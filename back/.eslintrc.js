9c7d019d8ed8478699b629b1d90131b83a990d24
Gaël Marcadet <gamarcadet@gmail.com>
2025-09-04 17:44:50 +0200

    dev: installation of eslint and prettire

---
+++ back/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    rules: {
+        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+};
